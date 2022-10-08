41276b6e690e2b478f7a177e2094e397381add01
amigin <amigin@gmail.com>
2022-10-08 17:52:21 +0300

    Added logging to publisher

--- src/publisher/publisher.rs
+++ src/publisher/publisher.rs
@@ -1,26 +1,31 @@
 use std::{collections::HashMap, sync::Arc};
 
+use rust_extensions::Logger;
+
 use crate::{MyServiceBusPublisherClient, PublishError};
 
 use super::{MessageToPublish, MySbMessageSerializer};
 
 pub struct MyServiceBusPublisher<TContract: MySbMessageSerializer> {
-    pub topic_name: String,
+    pub topic_id: String,
     pub client: Arc<dyn MyServiceBusPublisherClient + Send + Sync + 'static>,
     pub do_retries: bool,
     pub itm: Option<TContract>,
+    pub logger: Arc<dyn Logger + Send + Sync + 'static>,
 }
 
 impl<TContract: MySbMessageSerializer> MyServiceBusPublisher<TContract> {
     pub fn new(
-        topic_name: String,
+        topic_id: String,
         client: Arc<dyn MyServiceBusPublisherClient + Send + Sync + 'static>,
         do_retries: bool,
+        logger: Arc<dyn Logger + Send + Sync + 'static>,
     ) -> Self {
         Self {
-            topic_name,
+            topic_id,
             client,
             do_retries,
+            logger,
             itm: None,
         }
     }
@@ -29,18 +34,35 @@ impl<TContract: MySbMessageSerializer> MyServiceBusPublisher<TContract> {
         let content = message.serialize(None);
 
         if let Err(err) = content {
+            let mut ctx = HashMap::new();
+            ctx.insert("topicId".to_string(), self.topic_id.to_string());
+            self.logger
+                .write_fatal_error("publish".to_string(), err.clone(), Some(ctx));
             return Err(PublishError::SerializationError(err));
         }
 
         let (content, headers) = content.unwrap();
 
-        self.client
+        let result = self
+            .client
             .publish_message(
-                &self.topic_name,
+                &self.topic_id,
                 MessageToPublish { headers, content },
                 self.do_retries,
             )
-            .await
+            .await;
+
+        if let Err(err) = &result {
+            let mut ctx = HashMap::new();
+            ctx.insert("topicId".to_string(), self.topic_id.to_string());
+            self.logger.write_error(
+                "publish".to_string(),
+                format!("Can not publish message. Error: {:?}", err),
+                Some(ctx),
+            );
+        }
+
+        result
     }
 
     pub async fn publish_with_headers(
@@ -51,18 +73,39 @@ impl<TContract: MySbMessageSerializer> MyServiceBusPublisher<TContract> {
         let content = message.serialize(Some(headers));
 
         if let Err(err) = content {
+            let mut ctx = HashMap::new();
+            ctx.insert("topicId".to_string(), self.topic_id.to_string());
+            self.logger.write_fatal_error(
+                "publish_with_headers".to_string(),
+                err.clone(),
+                Some(ctx),
+            );
+
             return Err(PublishError::SerializationError(err));
         }
 
         let (content, headers) = content.unwrap();
 
-        self.client
+        let result = self
+            .client
             .publish_message(
-                &self.topic_name,
+                &self.topic_id,
                 MessageToPublish { headers, content },
                 self.do_retries,
             )
-            .await
+            .await;
+
+        if let Err(err) = &result {
+            let mut ctx = HashMap::new();
+            ctx.insert("topicId".to_string(), self.topic_id.to_string());
+            self.logger.write_error(
+                "publish_messages".to_string(),
+                format!("Can not publish message. Error: {:?}", err),
+                Some(ctx),
+            );
+        }
+
+        result
     }
 
     pub async fn publish_messages(&self, messages: &[TContract]) -> Result<(), PublishError> {
@@ -72,6 +115,14 @@ impl<TContract: MySbMessageSerializer> MyServiceBusPublisher<TContract> {
             let content = message.serialize(None);
 
             if let Err(err) = content {
+                let mut ctx = HashMap::new();
+                ctx.insert("topicId".to_string(), self.topic_id.to_string());
+                self.logger.write_fatal_error(
+                    "publish_messages".to_string(),
+                    err.clone(),
+                    Some(ctx),
+                );
+
                 return Err(PublishError::SerializationError(err));
             }
 
@@ -80,9 +131,22 @@ impl<TContract: MySbMessageSerializer> MyServiceBusPublisher<TContract> {
             messages_to_publish.push(MessageToPublish { headers, content });
         }
 
-        self.client
-            .publish_messages(&self.topic_name, messages_to_publish, self.do_retries)
-            .await
+        let result = self
+            .client
+            .publish_messages(&self.topic_id, messages_to_publish, self.do_retries)
+            .await;
+
+        if let Err(err) = &result {
+            let mut ctx = HashMap::new();
+            ctx.insert("topicId".to_string(), self.topic_id.to_string());
+            self.logger.write_error(
+                "publish_messages".to_string(),
+                format!("Can not publish message. Error: {:?}", err),
+                Some(ctx),
+            );
+        }
+
+        result
     }
 
     pub async fn publish_messages_with_header(
@@ -95,6 +159,14 @@ impl<TContract: MySbMessageSerializer> MyServiceBusPublisher<TContract> {
             let content = contract.serialize(headers);
 
             if let Err(err) = content {
+                let mut ctx = HashMap::new();
+                ctx.insert("topicId".to_string(), self.topic_id.to_string());
+                self.logger.write_fatal_error(
+                    "publish_messages_with_header".to_string(),
+                    err.clone(),
+                    Some(ctx),
+                );
+
                 return Err(PublishError::SerializationError(err));
             }
 
@@ -103,8 +175,21 @@ impl<TContract: MySbMessageSerializer> MyServiceBusPublisher<TContract> {
             messages_to_publish.push(MessageToPublish { content, headers });
         }
 
-        self.client
-            .publish_messages(&self.topic_name, messages_to_publish, self.do_retries)
-            .await
+        let result = self
+            .client
+            .publish_messages(&self.topic_id, messages_to_publish, self.do_retries)
+            .await;
+
+        if let Err(err) = &result {
+            let mut ctx = HashMap::new();
+            ctx.insert("topicId".to_string(), self.topic_id.to_string());
+            self.logger.write_error(
+                "publish_messages_with_header".to_string(),
+                format!("Can not publish message. Error: {:?}", err),
+                Some(ctx),
+            );
+        }
+
+        result
     }
 }
